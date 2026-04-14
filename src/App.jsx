15bbab96643a2cf2fadd1d01db67756c85fc8a75
ekin can <algunekincan@gmail.com>
2026-04-15 00:17:00 +0300

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>MergeMind</h1>;
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, './src/app'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@context': path.resolve(__dirname, './src/context'),
+    },
+  },
+})
